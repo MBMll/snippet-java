e5372f5f0f154f1e4479a9ff0e66a8682e6ae1f3
aulor.xe <aulor.xe@outlook.com>
2023-02-23 23:38:36 +0800

    add test class

---
+++ utils/src/test/java/com/github/mbmll/snippet/utils/BigDecimalUtilsTest.java
@@ -0,0 +1,30 @@
+package com.github.mbmll.snippet.utils;
+
+import junit.framework.TestCase;
+
+/**
+ * @Author xlc
+ * @Description
+ * @Date 2023/2/23 23:38
+ */
+
+public class BigDecimalUtilsTest extends TestCase {
+
+    public void testMultiply() {
+    }
+
+    public void testTestMultiply() {
+    }
+
+    public void testDivide() {
+    }
+
+    public void testTestDivide() {
+    }
+
+    public void testTestDivide1() {
+    }
+
+    public void testTestDivide2() {
+    }
+}
