2d518fe9433e8edf86500e0edfd5020144d812fd
aulor.xe <aulor.xe@outlook.com>
2023-07-10 00:29:04 +0800

    test CopyOnWriteArrayList

---
+++ java-se/jdk8/src/test/java/com/github/mbmll/snippet/jdk8/ThreadLocalTest.java
@@ -0,0 +1,24 @@
+package com.github.mbmll.snippet.jdk8;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.junit.Test;
+
+/**
+ * @Author xlc
+ * @Description
+ * @Date 2023/7/9 12:31
+ */
+
+public class ThreadLocalTest {
+
+  /**
+   * 内部数组扩展
+   */
+  @Test
+  public void expand() {
+    Map<String,String> map = new ConcurrentHashMap<>();
+    map.put("foo", "bar");
+  }
+
+}
