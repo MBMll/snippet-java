cc65e0d2fbdf7ba86f24cbc58442af302544ded9
aulor.xe <aulor.xe@outlook.com>
2023-11-05 01:35:09 +0800

    add state

---
+++ design-pattern/src/main/java/com/github/mbmll/design/pattern/behavior/state/State.java
@@ -0,0 +1,14 @@
+package com.github.mbmll.design.pattern.behavior.state;
+
+/**
+ * @Author xlc
+ * @Description
+ * @Date 2023/11/4 18:31:20
+ */
+
+public interface State {
+
+  void stop();
+
+  void move();
+}
